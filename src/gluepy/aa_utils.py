f24ba5296bbbecd3ad9a22a23d3b2000fadfb6e9
Josh Singer <josh.singer@glasgow.ac.uk>
2020-09-10 15:12:55 +0100

    comments

--- src/gluepy/aa_utils.py
+++ src/gluepy/aa_utils.py
@@ -3,7 +3,7 @@ Created on 8 Sep 2020
 
 @author: joshsinger
 '''
-
+# standard text format for each possible codon table
 # see https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=cgencodes
 standard_codon_table = """
   AAs  = FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
@@ -17,6 +17,7 @@ concrete_nt_triplet_to_aa = {}
 aa_to_concrete_nt_triplets = {}
 
 def init_from_codon_table(codon_table):
+	# find the 4 relevant lines from the table
 	lines = codon_table.splitlines()
 	base1_line = next(line for line in lines if "Base1" in line)
 	base1_line = base1_line[base1_line.rfind(" ")+1:]
@@ -26,6 +27,7 @@ def init_from_codon_table(codon_table):
 	base3_line = base3_line[base3_line.rfind(" ")+1:]
 	aas_line = next(line for line in lines if ("AAs" in line))
 	aas_line = aas_line[aas_line.rfind(" ")+1:]
+	# populate maps in both directions
 	for i in range(0,64):
 		nt_triplet = "".join([base1_line[i], base2_line[i], base3_line[i]])
 		aa = aas_line[i]
